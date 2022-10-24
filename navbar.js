0dab0090f2840897ceed5ea3d1b692a49ee73762
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-10-24 21:28:34 +0300

    duzenleme

--- navbar.js
+++ navbar.js
@@ -8,12 +8,5 @@ const NAVBAR =
     </a>
 </div>`
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "top-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-nav";
-    }
-}
+
 
