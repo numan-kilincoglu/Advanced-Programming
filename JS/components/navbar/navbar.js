0d1ce5c7b7b53c8a2295381002a1e8efbf9e774e
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-10-24 20:05:20 +0300

    nav

--- JS/components/navbar/navbar.js
+++ JS/components/navbar/navbar.js
@@ -1,37 +1,19 @@
 "use strict";
-const NAVBAR = 
-`<link href="https://maeyler.github.io/JS/navbar.css" rel="stylesheet">
-<ul>
-    <li class=Course>Course
-      <div class=menu>
-        <a href="/2022/index.html">Home 🏠</a>
-        <a href="/2022/Course_outline.html">Outline</a>
-        <a href="https://eloquentjavascript.net/" target=BLM305>Textbook</a>
-        <a href="https://github.com/BLM305/2022" target=BLM305>Repository</a>
-      </div>
-    </li>
-</ul>`
+const NAVBAR =
+    `<link href="https://nkilincoglu.github.io/Advanced-Programming/JS/css/navbar.css" rel="stylesheet">
+<div class="top-nav" id="myTopnav">
+    <a href="#Advanced Programming" class="active">Home</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>`
 
-function setNavbar() {
-  let nav = document.createElement('nav')
-  nav.innerHTML = NAVBAR
-  nav.id = 'navbar'
-  document.body.append(nav)
-  nav.querySelectorAll("li").forEach(li => {
-    let div = li.firstElementChild
-    li.onmouseenter = () => {
-      div.style.display = 'block'
-      let w = div.offsetWidth - li.offsetWidth
-      let x = li.offsetLeft - w/2
-      let m = nav.offsetWidth - div.offsetWidth
-      div.style.left = Math.min(Math.max(0, x), m)+'px'
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
-    li.onmouseleave = () => { div.style.display = '' }
-  })
 }
-setNavbar()
-onload = () => {
-  let f = document.querySelector('div.footer')
-  if (!f) return
-  f.remove(); console.log('remove footer')
-}
+
