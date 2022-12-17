28ea54f8b507addd24f4c47119f7048fad9fe3e0
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-12-18 00:59:31 +0300

    navbar yapildi

--- CW/TermProject/script.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelector('.circle').addEventListener('click', () => {
-    document.querySelector('.vertical').classList.toggle('open');
-    document.querySelector('.line-wrapper').classList.toggle('open');
-    document.querySelector('.content').classList.toggle('open');
-  });
