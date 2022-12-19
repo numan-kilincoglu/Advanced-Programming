5a7fc75f36573c5169a95f7e17387fd5965d79c7
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-12-20 00:11:34 +0300

    grid uzerinde calisma

---
+++ CW/TermProject/script.js
@@ -0,0 +1,42 @@
+
+const itemArray =
+    [
+        {
+            "name": "M9 Bayonet Doppler",
+            "price": "1300 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+        {
+            "name": "AWP Dragon Lore",
+            "price": "1300 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+        {
+            "name": "AWP Silk Tiger",
+            "price": "131100 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+        {
+            "name": "purple",
+            "price": "1500 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+        {
+            "name": "purple",
+            "price": "1000 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+        {
+            "name": "purple",
+            "price": "1100 $",
+            "img": "src/",
+            "game": "cs:go"
+        },
+    ];
+
+
