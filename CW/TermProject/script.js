70765f5aac88e4d646bfbeffd79749b6f51d8b3d
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-12-25 19:26:08 +0300

    pwa

--- CW/TermProject/script.js
+++ CW/TermProject/script.js
@@ -1,3 +1,14 @@
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("sw registered!");
+        console.log(registration);
+    }).catch(err => {
+        console.log("sw failed!");
+        console.log(err);
+    })
+}
+
 let gameType = "csgo";
 let currency = "usd";
 let lang = "en";
