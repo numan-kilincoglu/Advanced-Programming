3a4a8e86f5487a47b42522fe5b4a829502d981a0
nkilincoglu <numan.kilincoglu@stu.fsm.edu.tr>
2022-12-25 21:11:00 +0300

    sw

--- CW/TermProject/sw.js
+++ CW/TermProject/sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open("static").then(
             cache => {
-                return cache.addAll(["./index.html", "./style.css", "./assets/", "./script.js", "./assets/game-icon.PNG"]);
+                return cache.addAll(["./","./index.html", "./style.css", "./assets/", "./script.js", "./assets/game-icon.PNG"]);
 
             }
         )
